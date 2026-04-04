69f41ea0e7b8747904c44e6c5b5e87fb9984bc1d
ProgrammerMahediHasan <mahedihasanabir8@gmail.com>
2026-04-04 23:44:09 +0600

    learn-javascript

---
+++ Js_syntax.js
@@ -0,0 +1,15 @@
+//  let variable 
+
+let fruit="mango";
+
+console.log(`${fruit} is my fav fruit`);
+
+
+
+// var variable 
+
+var city='Dhaka';
+
+console.log(`${city} is the biggest city in Bangladesh`);
+
+
