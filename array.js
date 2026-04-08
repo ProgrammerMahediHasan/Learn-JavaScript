244b3d7afeb5e35f432b4ede079e6bdb72a9452f
ProgrammerMahediHasan <mahedihasanabir8@gmail.com>
2026-04-08 12:17:21 +0600

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+var employees = ['Mahedi','Anamul','Abdullah','Pollob'];
+
+console.log(employees);
+
+// indexarray 
+
+var friendsAge=[20,30,40,50];
+
+var age=friendsAge[3];
+
+// console.log(friendsAge.indexOf(50));
+console.log(friendsAge[3]);
+
+// lengtharray 
+
+var employeeName=["Mahedi Hasan", "Pollob Sagor", "Anamul Haque", "Abdullah", "Harun"];
+
+
+console.log(employeeName.length)
--- node.js
+++ node.js
@@ -1,3 +1,5 @@
 console.log('this is my javascript project');
 
-document.write('this is my javascript project');
+// document.write('this is my javascript project');
+
+// alert('are you want to delete this file?');
