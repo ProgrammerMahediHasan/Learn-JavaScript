69a86c08f1b9f89072442aae06d9793767f8651e
ProgrammerMahediHasan <mahedihasanabir8@gmail.com>
2026-04-08 18:14:54 +0600

    array

--- array.js
+++ array.js
@@ -1,19 +1,54 @@
-var employees = ['Mahedi','Anamul','Abdullah','Pollob'];
+// var employees = ['Mahedi','Anamul','Abdullah','Pollob'];
 
-console.log(employees);
+// console.log(employees);
 
 // indexarray 
 
-var friendsAge=[20,30,40,50];
+// var friendsAge=[20,30,40,50];
 
-var age=friendsAge[3];
+// var age=friendsAge[3];
 
-// console.log(friendsAge.indexOf(50));
-console.log(friendsAge[3]);
+// // console.log(friendsAge.indexOf(50));
+// console.log(friendsAge[3]);
 
 // lengtharray 
 
-var employeeName=["Mahedi Hasan", "Pollob Sagor", "Anamul Haque", "Abdullah", "Harun"];
+// var employeeName=["Mahedi Hasan", "Pollob Sagor", "Anamul Haque", "Abdullah", "Harun"];
 
 
-console.log(employeeName.length)
+// console.log(employeeName.length)
+
+
+// get/set arraylist 
+
+// var play= ['cricket','football','tennis','galf'];
+// play[4]='wrestling';
+
+// console.log(play);
+
+
+// push & pop  array
+
+// var employees=['Emon','Jahid','Rifat'];
+// // employees.push=['Mahedi','Pollob']
+// employees.push('Mahedi','Pollob');
+
+// employees.pop();
+// employees.pop();
+// console.log(employees);
+
+// shift unshift array 
+
+// var game=['cricket','football','tennis'];
+
+// game.unshift('wrestling');
+// game.shift();
+
+// console.log(game);
+
+// sort array 
+
+// var number=[2,1,4,3,5];
+// var employeename=['Biplop','Abdullah','Chimul'];
+// employeename.sort();
+// console.log(employeename);
